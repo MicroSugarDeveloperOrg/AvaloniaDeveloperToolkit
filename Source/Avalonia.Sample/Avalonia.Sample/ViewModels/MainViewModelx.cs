7128c2ff656859f8448514965c02e805f36a076a
MicroSugar <wufeng4728704@hotmail.com>
2024-01-29 16:02:17 +0800

    remove

--- Source/Avalonia.Sample/Avalonia.Sample/ViewModels/MainViewModelx.cs
+++ Source/Avalonia.Sample/Avalonia.Sample/ViewModels/MainViewModelx.cs
@@ -1,12 +1,4 @@
 ﻿using ReactiveUI;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reactive;
-using System.Reactive.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
 
 namespace Avalonia.Sample.ViewModels;
 
