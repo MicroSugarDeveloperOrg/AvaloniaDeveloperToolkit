30a6322317e9bc911cd852ed678c8a59164fb2bf
MicroSugar <wufeng4728704@hotmail.com>
2024-01-30 11:31:57 +0800

    add new extensions

---
+++ Source/SourceGeneratorToolkit.Shared/Builders/CodeBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace SourceGeneratorToolkit.Builders;
+
+internal static class CodeBuilderExtensions
+{
+    public static CodeBuilder AppendBaseType(this CodeBuilder builder, string baseType)
+    {
+        var bRet = builder.AppendBaseType(baseType);
+        return builder;
+    }
+
+    public static CodeBuilder AppendInterface(this CodeBuilder builder, string interfaceName)
+    {
+        var bRet = builder.AppendInterface(interfaceName);
+        return builder;
+    }
+
+    public static CodeBuilder AppendProperty(this CodeBuilder builder, string type, string fieldName, string propertyName)
+    {
+        var bRet = builder.AppendProperty(type, fieldName, propertyName);
+        return builder;
+    }
+
+    public static CodeBuilder AppendCommand(this CodeBuilder builder, string? argumentType, string? returnType, string methodName, string? canMethodName)
+    {
+        var bRet = builder.AppendCommand(argumentType, returnType, methodName, canMethodName);
+        return builder;
+    }
+}
