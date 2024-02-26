ff18fd84d1c4c794ea75227e2a9d1bfdf0abe0e1
MicroSugar <wufeng4728704@hotmail.com>
2024-02-26 21:00:28 +0800

    update to 0.0.2

--- Nuget.bat
+++ Nuget.bat
@@ -3,3 +3,4 @@ echo b = %2
 
 dotnet nuget push nuget\PrismToolkit.SourceGenerators.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json 
 dotnet nuget push nuget\Rx.SourceGenerators.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json
+dotnet nuget push nuget\Mvvm.SourceGenerators.%1.nupkg --api-key %2 --source https://api.nuget.org/v3/index.json
